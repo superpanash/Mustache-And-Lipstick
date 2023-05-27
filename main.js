a3a59afe19e3923788e0ff5989103491e618e7f3
superpanash <110617003+superpanash@users.noreply.github.com>
2023-05-27 15:24:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(350,350);
+canvas.center();
+
+}
+
+function draw(){
+
+}
+
+function snapshot(){
+    save("Moustache/Lipstick_filter.png");
+}
