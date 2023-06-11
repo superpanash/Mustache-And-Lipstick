ae432c6216faa30b16cfa6c85fc446532938ed33
superpanash <110617003+superpanash@users.noreply.github.com>
2023-06-11 12:42:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,12 +6,34 @@ function setup(){
 canvas=createCanvas(350,350);
 canvas.center();
 
+video=createCapture(VIDEO);
+video.size(350,350)
+video.hide();
+
+posenet=ml5.poseNet(video,modelLoaded)
+
+posenet.on("pose",gotPoses)
+
 }
 
-function draw(){
 
+function draw(){
+image(video,0,0,350,350);
 }
 
 function snapshot(){
     save("Moustache/Lipstick_filter.png");
+}
+
+
+function modelLoaded(){
+    console.log("POSENET IS iNITIALIZED")
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("nose x ="+results[0].pose.nose.x);
+        console.log("nose y ="+results[0].pose.nose.y);
+    }
 }
