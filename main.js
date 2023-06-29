4124da6fcd39693092ce6edd53ce8d2dbe1c2df0
superpanash <110617003+superpanash@users.noreply.github.com>
2023-06-29 20:13:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+var nose_x=0;
+var nose_y=0;
 
+function preload(){
+mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
@@ -7,18 +10,18 @@ canvas=createCanvas(350,350);
 canvas.center();
 
 video=createCapture(VIDEO);
-video.size(350,350)
+video.size(350,350);
 video.hide();
 
-posenet=ml5.poseNet(video,modelLoaded)
+posenet=ml5.poseNet(video,modelLoaded);
 
-posenet.on("pose",gotPoses)
+posenet.on("pose",gotPoses);
 
 }
 
-
 function draw(){
 image(video,0,0,350,350);
+image(mustache,nose_x,nose_y,50,30)
 }
 
 function snapshot(){
@@ -33,7 +36,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x ="+results[0].pose.nose.x);
-        console.log("nose y ="+results[0].pose.nose.y);
+        nose_x=results[0].pose.nose.x-20;
+        nose_y=results[0].pose.nose.y+5;
+        console.log("Nose x ="+nose_x);
+        console.log("nose y ="+nose_y);
     }
 }
